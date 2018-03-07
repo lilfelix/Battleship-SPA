e9bf31cc585f62750a00a22d66f974989043924d
Felix <felixlil@kth.se>
2018-03-07 11:20:03 +0100

    create skeleton for websocket service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { WebsocketService } from './websocket.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [WebsocketService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/src/app/websocket.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { WebSocketSubject } from 'rxjs/observable/dom/WebSocketSubject';
+
+@Injectable()
+export class WebsocketService {
+
+  // TODO socket handles login, game and chat objects
+  public socket$: WebSocketSubject<any>;
+  private serverData: any[] = [];
+
+  constructor(
+    // private gameService: GameService,
+    ) {
+    this.socket$ = WebSocketSubject.create('ws://localhost:3000');
+
+    this.socket$
+      .subscribe(
+        (message) => { // message is a JSON object
+          this.serverData.push(message);
+          console.log('websocketservice received from server: '); // obj.message);
+          console.dir(message);
+          // const arr = JSON.parse(message);
+          message.forEach(object => {
+            console.dir(object);
+            switch (object.status) {
+             default:
+                console.log('Unidentified message from websocket:');
+                console.dir(message);
+            }
+          });
+        },
+        (err) => console.error(err),
+        () => console.warn('Completed!')
+      );
+
+  }
+
+}
+
