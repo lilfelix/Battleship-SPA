366c7ed0354f8bfd6ed166a63912e7f01602a5c3
Felix <felixlil@kth.se>
2018-03-07 13:33:41 +0100

    work on login component

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,8 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  username = '';
+  public username: string;
+  private password: string;
   loggedIn = false;
 
   constructor() { }
@@ -15,8 +17,7 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
-  onEnter(username: string) {
-    this.username = username;
+  onSubmit() {
     this.loggedIn = true;
   }
 }
