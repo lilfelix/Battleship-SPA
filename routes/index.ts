53013cb63b5b64b8b7a2e0637186fa1069896803
Felix <felixlil@kth.se>
2018-03-09 09:37:59 +0100

    send json web token on login authentication

--- routes/index.ts
+++ routes/index.ts
@@ -3,15 +3,28 @@ import path from "path";
 import bodyParser from "body-parser";
 import cookieParser from 'cookie-parser';
 import errorHandler from "errorhandler";
-import 'jsonwebtoken';
-import jwt from 'express-jwt';
+import jwt from 'jsonwebtoken';
+// import jwt from 'express-jwt';
 import "reflect-metadata";
 
 export const router = express.Router();
 
-router.get('/login', function(req, res) {
-    const obj = {success: true, token: 'exampleToken'};
-    res.setHeader('Content-Type', 'application/json');
-    res.send(obj);
+router.get('/login', function (req, res) {
+    // if user is found and password is right
+    // create a token with only our given payload
+    // we don't want to pass in the entire user since that has the password
+    const payload = {
+        username: req.body.username
+    };
+
+    const token = jwt.sign(payload, '@9O37m1O3ISg', {
+        expiresIn: '1d' // expires in 24 hours
+    });
+
+    // return the information including token as JSON
+    res.json({
+        success: true,
+        token: token
+    });
 });
 
