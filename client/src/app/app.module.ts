a547a072c591f93c9024191d129417a13966800b
Felix <felixlil@kth.se>
2018-03-09 12:31:30 +0100

    add lobby component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,18 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { WebsocketService } from './websocket.service';
 import { LoginService } from './login/login.service';
+import { LobbyComponent } from './lobby/lobby.component';
+
+const appRoutes: Routes = [
+  { path: 'lobby',
+    component: LobbyComponent,
+    data: { title: 'Lobby'}
+  },
+  { path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    LobbyComponent
   ],
   imports: [
     BrowserModule,
